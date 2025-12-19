dd871d61c302b88dfcdde87eb515e8cf427d37f6
키위 <slalom0914@gmail.com>
2025-12-19 14:34:22 +0900

    프론트앤드라우터추가

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,4 +1,6 @@
+import { Route, Routes } from 'react-router';
 import './App.css';
+import HomePage from './components/pages/HomePage';
 //리액트에서는 태그를 만들어 쓴다.
 //주의: 콤포넌트의 이름을 반드시 대문자로 한다.
 //화살표 함수로 변경한 부분
@@ -7,9 +9,13 @@ import './App.css';
 const App = () => {
   //선언부
   return (
-    <div className="App">
-      Hello React!!!
-    </div>
+    <>
+      <div style={{ height: '100vh'}}>
+        <Routes>
+          <Route path='/' exact={true} element={<HomePage />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
