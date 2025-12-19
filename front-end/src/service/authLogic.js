ba8c01a5879677517fe1d9ec9690e22c0726483a
키위 <slalom0914@gmail.com>
2025-12-19 16:57:28 +0900

    7교시

--- front-end/src/service/authLogic.js
+++ front-end/src/service/authLogic.js
@@ -1,5 +1,20 @@
-class AuthLogic {
-
+import {
+  GoogleAuthProvider,
+  getAuth,
+} from "firebase/auth";
+class AuthLogic {//클래스 선언
+  constructor() {
+    this.auth = getAuth();//firebase developer console신청한 프로젝트 설정정보확인
+    this.googleProvider = new GoogleAuthProvider();//구글인증, 카카오인증, 깃헙인증
+    //this.kakaoProvider = new KakaoAuthProvider();
+    //this.githubProvider = new GihubAuthProvider();
+  }
+  //auth반환하는 함수 선언
+  getUserAuth = () => {
+    return this.auth;
+  };
+  getGoogleAuthProvider = () => {
+    return this.googleProvider;//google
+  };
 }
-
-export default AuthLogic
+export default AuthLogic; //외부 js에서 사용할 때
